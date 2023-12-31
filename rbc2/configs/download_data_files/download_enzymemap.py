102cc4ca59d720be5f9e543c1a3824c272908f8c
willfinnigan <wjafinnigan@gmail.com>
2023-12-31 18:36:39 +0000

    enzymemap autodownload

--- rbc2/configs/download_data_files/download_enzymemap.py
+++ rbc2/configs/download_data_files/download_enzymemap.py
@@ -13,10 +13,22 @@ def download_enzymemap():
     directory = f"{path_to_data_folder}/enzymemap/brenda"
     os.makedirs(directory, exist_ok=True)
 
+    file_id = "1CNS8AZ49z3V24zXgIJoqh2EWWGtxNPXM"
+    filename = "enzymemap_brenda_metadata.csv"
+    filepath = f"{directory}/{filename}"
+    download_gdrive_file(file_id, filepath)
+
+    file_id = "1CSE1ZszPvhhrRUXJU4YZC9WJSeqF9pXt"
+    filename = "enzymemap_brenda_metadata.csv"
+    filepath = f"{directory}/{filename}"
+    download_gdrive_file(file_id, filepath)
+
     file_id = "1CAGlHEp4Cnbl_FgepIdXYRCMHd562WDv"
     filename = "default_retro-weights.hdf5"
     filepath = f"{directory}/{filename}"
     download_gdrive_file(file_id, filepath)
+
+
 
 if __name__ == '__main__':
     download_enzymemap()
