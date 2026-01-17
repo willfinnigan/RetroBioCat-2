b2556dbb92118bd79fe64d46bd26e0b2a52d2be4
willfinnigan <wjafinnigan@gmail.com>
2026-01-17 18:21:08 +0000

    updates to higher versions

--- rbc2/utils/load_keras_models.py
+++ rbc2/utils/load_keras_models.py
@@ -43,19 +43,44 @@ class LocalKerasModel:
     def __init__(self, filename):
         load_model, CUSTOM_OBJECTS = tensorflow_imports()
 
-        self.model = load_model(filename, custom_objects=CUSTOM_OBJECTS)
-        try:
-            self._model_dimensions = int(self.model.input.shape[1])
-        except AttributeError:
-            self._model_dimensions = int(self.model.input[0].shape[1])
-        self.output_size = int(self.model.output.shape[1])
+        self.model = load_model(filename, custom_objects=CUSTOM_OBJECTS, compile=False)
+
+        # Detect model type for Keras 3 compatibility
+        self._is_sequential = self.model.__class__.__name__ == 'Sequential'
+
+        # Get dimensions from layer weights (skip layers without weights like InputLayer)
+        first_layer_with_weights = None
+        last_layer_with_weights = None
+        for layer in self.model.layers:
+            weights = layer.get_weights()
+            if weights:
+                if first_layer_with_weights is None:
+                    first_layer_with_weights = weights[0]
+                last_layer_with_weights = weights[0]
+
+        self._model_dimensions = int(first_layer_with_weights.shape[0]) if first_layer_with_weights is not None else 2048
+        self.output_size = int(last_layer_with_weights.shape[1]) if last_layer_with_weights is not None else 1
 
     def __len__(self):
         return self._model_dimensions
 
-    def predict(self,  *args: np.ndarray, **_: np.ndarray):
-        result = self.model(args)  # this is by far the fastest
-        #result = self.model.predict(args)
-        #gc.collect()
+    def predict(self, *args: np.ndarray, **kwargs: np.ndarray):
+        # Handle multi-input models (functional API)
+        if len(args) > 1 or kwargs:
+            # Multi-input model - pass inputs directly
+            if kwargs:
+                result = self.model.predict(kwargs, verbose=0)
+            else:
+                result = self.model.predict(list(args), verbose=0)
+            return result
+
+        # Single input - may need dimension adjustment for Sequential models saved with Keras 2
+        inputs = args[0]
+        if self._is_sequential and isinstance(inputs, np.ndarray) and inputs.ndim == 2:
+            inputs = np.expand_dims(inputs, axis=1)
+        result = self.model.predict(inputs, verbose=0)
+        # Squeeze back to 2D if we added a dimension
+        if result.ndim == 3 and result.shape[1] == 1:
+            result = np.squeeze(result, axis=1)
         return result
 
